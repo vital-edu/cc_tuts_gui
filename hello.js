f3ea3404be8e6ede33672a507694b0bf04fe2461
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-11-03 23:51:44 -0300

    create and retrieve person

--- hello.js
+++ hello.js
@@ -5,12 +5,43 @@ var holochain_connection = holochainclient.connect({
 function hello() {
   holochain_connection.then(({ callZome, close }) => {
     callZome('test-instance', 'hello', 'hello_holo')({ args: {} })
-      .then((result) => showOutput(result))
+      .then((result) => showOutput(result, 'output'))
   })
 }
 
-function showOutput(text) {
-  let span = document.getElementById('output')
-  let parsedText = JSON.parse(text)
-  span.textContent = parsedText.Ok;
+function showOutput(result, id) {
+  let el = document.getElementById(id)
+  let output = JSON.parse(result)
+  if (output.Ok) {
+    el.textContent = output.Ok;
+  } else {
+    alert(output.Err.Internal);
+  }
+}
+
+function showPerson(result) {
+  let el = document.getElementById('person_output')
+  let output = JSON.parse(result)
+  if (output.Ok) {
+    el.innerText = output.Ok.name
+  } else {
+    alert(output.Err.Internal);
+  }
+}
+
+function createPerson() {
+  holochain_connection.then(({ callZome, close }) => {
+    let name = document.getElementById('name').value
+    callZome('test-instance', 'hello', 'create_person')({ person: { name } })
+      .then((result) => showOutput(result, 'address_output'))
+  })
+}
+
+function retrievePerson() {
+  let address = document.getElementById('address_in').value
+  holochain_connection.then(({ callZome, close }) => {
+    callZome('test-instance', 'hello', 'retrieve_person')({
+      address
+    }).then((result) => showPerson(result))
+  })
 }
